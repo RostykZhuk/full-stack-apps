e5aa5376bcd64c1404137c4c097fe72c6e5f46df
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-07-15 08:01:18 +0300

    Sign In & Sign Up form

---
+++ memories_project/client/src/components/Auth/Input.js
@@ -0,0 +1,43 @@
+import { Grid, IconButton, InputAdornment, TextField } from "@material-ui/core";
+import React from "react";
+import { VisibilityOff, Visibility } from "@material-ui/icons";
+
+const Input = ({
+  half,
+  name,
+  label,
+  type,
+  handleChange,
+  autoFocus,
+  handleShowPassword,
+}) => {
+  return (
+    <Grid item xs={12} sm={half ? 6 : 12}>
+      <TextField
+        name={name}
+        onChange={handleChange}
+        variant="outlined"
+        required
+        fullWidth
+        label={label}
+        autoFocus={autoFocus}
+        type={type}
+        InputProps={
+          name === "password"
+            ? {
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton onClick={handleShowPassword}>
+                      {type === "password" ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }
+            : null
+        }
+      />
+    </Grid>
+  );
+};
+
+export default Input;
